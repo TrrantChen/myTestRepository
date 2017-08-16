5183af086965d1ea3cdeab80c0e082f5cbc80204
TarrantChen <shiva0622@gmail.com>
2017-08-16 23:16:35 +0800

    test

---
+++ script/resetnode.bat
@@ -0,0 +1,4 @@
+npm config set prefix "D:\npm\npm_global"
+npm config set cache "D:\npm\npm_cache"
+npm config set proxy "http://127.0.0.1:10086"
+npm config set https-proxy "https://127.0.0.1:10086"
