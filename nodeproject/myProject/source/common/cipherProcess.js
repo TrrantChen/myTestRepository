bcbd7776a01e846aad66da3dc0c0f3ae1fc1244d
TarrantChen <shiva0622@gmail.com>
2017-09-07 17:15:08 +0800

    test

---
+++ nodeproject/myProject/source/common/cipherProcess.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
+fs.readFile("./key/rsa_private.key", 'utf-8', (err, data) => {
+  if (err) {
+    console.log(data);
+  } else {
+    
+  }
+})
