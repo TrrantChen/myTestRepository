df28e3b4110f70c43ccf3d35a51a756265f3e5cd
TarrantChen <shiva0622@gmail.com>
2017-09-07 17:20:04 +0800

    test

---
+++ front/sourcecode/registerTest/src/registerTest.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+import  * as util from '../../js/common/util'; 
+import { getDomCount, action4EverySonDom, ButtonContent, setFrame, printDomTree} from '../../js/common/domoperation';
+
+$(() => {
+  
+})
